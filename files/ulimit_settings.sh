80c2b3cf9bdda4a3483c399bb146027d204e02b3
Oleg Vivtash <oleg@vivtash.net>
2015-05-15 21:09:26 +0300

    Adjusting ulimits

---
+++ files/ulimit_settings.sh
@@ -0,0 +1,8 @@
+# http://docs.mongodb.org/manual/reference/ulimit/#recommended-ulimit-settings
+
+ulimit -f unlimited
+ulimit -t unlimited
+ulimit -v unlimited
+ulimit -n 64000
+ulimit -m unlimited
+ulimit -u 64000
